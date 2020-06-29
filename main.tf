12a2d179f77859136a04eb0670890d37a8819414
Marcel-G <gleeson.marcel@gmail.com>
2020-06-29 10:17:05 +0200

    Use public dns instead of ip

--- main.tf
+++ main.tf
@@ -185,6 +185,10 @@ output "instance_ip" {
   value = aws_spot_instance_request.windows_instance.public_ip
 }
 
+output "instance_public_dns" {
+  value = aws_spot_instance_request.windows_instance.public_dns
+}
+
 output "instance_password" {
   value = random_password.password.result
   sensitive = true
